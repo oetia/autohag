889e23e52c045b5d8059ab0df5facec5ed6192f2
0eti <samuelzhang012@gmail.com>
2023-08-03 16:31:12 +0800

    refactored callback hell with suspendCoroutine

--- app/src/main/java/com/magicalhag/autohag/InputService.kt
+++ app/src/main/java/com/magicalhag/autohag/InputService.kt
@@ -20,26 +20,25 @@ import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.Spinner
+import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import java.util.Timer
 import java.util.TimerTask
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 
 class InputService : AccessibilityService() {
 
-//    companion object {
-//        // Potential Memory Leak
-//        var serviceInstance: InputService? = null
-//        fun getInstance(): InputService? {
-//            return serviceInstance
-//        }
-//    }
-
     private lateinit var mLayout: FrameLayout;
 
-    val screenshotExecutor = ScreenshotExecutor();
+    private val screenshotExecutor = ScreenshotExecutor();
     val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
     val thread = Timer()
@@ -81,6 +80,7 @@ class InputService : AccessibilityService() {
 
 
         configurePowerButton()
+        configureScrollButton()
 //        configureOpenButton()
         configureShotButton()
         configureStartButton()
@@ -114,6 +114,27 @@ class InputService : AccessibilityService() {
         powerButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
 
+//                performGlobalAction(GLOBAL_ACTION_BACK)
+//                performGlobalAction(FOR)
+//                Get display information for how to send out and receive swipes and clicks.
+//                Can relly on OCR carry for pretty much everything else
+//                There isn't exactly a convenient swipe method available for usage, so I pretty much just have to make my own
+//                Might also have to calculate where to click based upon what one is seeing
+
+//                In order to tell if state has changed, can search for verification text in other textblocks
+//                I'm wondering what kind of datastructure i should use for quick searching a bunch of information
+//                I think that having some kind of a previous state and calculating a diff... since loading will often stall and not pop off quite properly.
+//                I need a better system for state management.
+//                Despite that it works, code is jank and bodged together.
+
+//                Side note, I need to get more and more familiar with using vim
+//                For vertical movement, just using shift J and shift K works well enough here
+//                My screen is usually pretty zoomed in, so using control U control D is not that necessary
+
+//                resources.displayMetrics.heightPixels
+
+
+                performGlobalAction(GLOBAL_ACTION_DPAD_DOWN)
                 val launchIntent = Intent().setComponent(
                     ComponentName(
                         "com.YoStarEN.Arknights", "com.u8.sdk.U8UnityContext"
@@ -157,20 +178,27 @@ class InputService : AccessibilityService() {
         })
     }
 
+    private fun configureShotButton() {
+        val shotButton = mLayout.findViewById(R.id.shot) as Button
+        shotButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) = runBlocking {
+                iteration2()
+            }
+        })
+    }
+
     private fun configureStartButton() {
         val startButton = mLayout.findViewById(R.id.start) as Button
         startButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
+            override fun onClick(v: View) {
                 threadPaused = false
                 if (!started) {
-                    Log.d(
-                        getString(R.string.log_tag),
-                        "Thread hasn't been started. Creating a new one. "
-                    )
+                    Log.d(getString(R.string.log_tag), "Thread hasn't been started. Creating a new one.")
                     thread.scheduleAtFixedRate(object : TimerTask() {
-                        override fun run() {
+                        override fun run() = runBlocking {
                             if (!threadPaused) {
-                                iteration()
+//                                iteration()
+                                iteration2()
 
                                 iterationCounter += 1
                                 Log.d(getString(R.string.log_tag), iterationCounter.toString())
@@ -179,10 +207,7 @@ class InputService : AccessibilityService() {
                     }, 0, 3000)
                     started = true
                 } else {
-                    Log.d(
-                        getString(R.string.log_tag),
-                        "Thread already started. Not creating a new one. "
-                    )
+                    Log.d(getString(R.string.log_tag), "Thread already started. Not creating a new one. ")
                 }
             }
         })
@@ -201,7 +226,79 @@ class InputService : AccessibilityService() {
 //    private
 //    private fun TextBlocks() {}
 
+    private suspend fun takeScreenshotSequential(): ScreenshotResult =
+        suspendCoroutine { continuation ->
+            takeScreenshot(
+                Display.DEFAULT_DISPLAY,
+                screenshotExecutor,
+                object : TakeScreenshotCallback {
+                    override fun onFailure(errorCode: Int) {
+                        continuation.resumeWithException(Exception("Screenshot Failed"))
+                    }
+
+                    override fun onSuccess(screenshot: ScreenshotResult) {
+                        continuation.resume(screenshot)
+                    }
+                })
+        }
+
+    private suspend fun recognizerProcessSequential(image: InputImage): Text =
+        suspendCoroutine { continuation ->
+            val task = recognizer.process(image)
+            while (!task.isSuccessful and !task.isComplete) {
+            } // this is so fucking jank.
+            continuation.resume(task.result)
+        }
+
+    suspend fun iteration2() { // suspend allows suspension of thread until completion
+        val screenshot = takeScreenshotSequential()
+        val bitmap = Bitmap.wrapHardwareBuffer(screenshot.hardwareBuffer, screenshot.colorSpace)
+        val image = InputImage.fromBitmap(bitmap!!, 0)
+        val visionText = recognizerProcessSequential(image)
+        Log.d(
+            getString(R.string.log_tag),
+            visionText.text
+        )
+
+        for (block in visionText.textBlocks) {
+            val blockText = block.text
+            val blockCornerPoints = block.cornerPoints
+            val blockCenter = getBoxCenter(blockCornerPoints as Array<Point>)
+
+//            Log.d(
+//                getString(R.string.log_tag),
+//                blockText + "\n" + blockCenter.toString() + "\n" + blockCornerPoints.contentDeepToString()
+//            )
+
+            if (
+                Regex("start\\s+-\\d{1,2}").containsMatchIn(blockText.lowercase()) ||
+                Regex("mission\\s+start").containsMatchIn(blockText.lowercase()) ||
+                Regex("mission\\s+results").containsMatchIn(blockText.lowercase())
+            ) {
+                dispatch(
+                    buildClick(
+                        blockCenter.x.toFloat(), blockCenter.y.toFloat(), 500
+                    )
+                )
+            } else if (Regex("takeover").containsMatchIn(blockText.lowercase())) {
+                Log.d(getString(R.string.log_tag), "Still In Autodeploy: PAUSING")
+                threadPaused = true
+                val unpause = Timer()
+                unpause.schedule(object : TimerTask() {
+                    override fun run() {
+                        Log.d(getString(R.string.log_tag), "UNPAUSING")
+                        threadPaused = false
+                    }
+                }, 1000 * 10)
+            } else if (Regex("restore").containsMatchIn(blockText.lowercase())) {
+                Log.d(getString(R.string.log_tag), "Sanity Gone: PAUSING")
+                threadPaused = true
+            }
+        }
+    }
+
     private fun iteration() {
+
         takeScreenshot(
             Display.DEFAULT_DISPLAY,
             screenshotExecutor,
@@ -282,6 +379,11 @@ class InputService : AccessibilityService() {
                                 )
 
                                 threadPaused = true
+                            } else {
+                                Log.d(
+                                    getString(R.string.log_tag),
+                                    blockText + "\n" + blockCenter.toString() + "\n" + blockCornerPoints.contentDeepToString()
+                                )
                             }
 
                         }
@@ -295,30 +397,23 @@ class InputService : AccessibilityService() {
             })
     }
 
-    private fun configureShotButton() {
-        val shotButton = mLayout.findViewById(R.id.shot) as Button
-        shotButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                iteration()
-            }
-        })
-    }
 
 
-    fun getBoxCenter(cornerPoints: Array<Point>): Point {
+    private fun getBoxCenter(cornerPoints: Array<Point>): Point {
         val centerX = (cornerPoints[1].x + cornerPoints[0].x) / 2
         val centerY = (cornerPoints[2].y + cornerPoints[1].y) / 2
         return Point(centerX, centerY)
     }
 
-    private fun configureSwipeButton() {
-        val swipeButton = mLayout.findViewById(R.id.swipe) as Button
-        swipeButton.setOnClickListener(object : View.OnClickListener {
+
+    private fun configureScrollButton() {
+        val scrollButton = mLayout.findViewById(R.id.scroll) as Button
+        scrollButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
-//                dispatch(buildSwipe(1000f, 1000f, 100f, 1000f, 500))
-                dispatch(buildClick(550f, 650f, 500))
+                dispatch(buildScroll("DOWN", 1000))
             }
         })
+
     }
 
     fun buildClick(x: Float, y: Float, duration: Long): GestureDescription {
@@ -329,6 +424,55 @@ class InputService : AccessibilityService() {
         gestureBuilder.addStroke(GestureDescription.StrokeDescription(clickPath, 0, duration))
 
         return gestureBuilder.build()
+    }
+
+
+    fun buildScroll(type: String, duration: Long): GestureDescription {
+
+        Log.d(getString(R.string.log_tag), "scrolling")
+
+        val screenHeight = resources.displayMetrics.heightPixels
+        val screenWidth = resources.displayMetrics.widthPixels
+
+        // indicates corners of a square
+        val topY = screenHeight * 0.25
+        val bottomY = screenHeight * 0.75
+        val leftX = screenWidth * 0.25
+        val rightX = screenWidth * 0.75
+
+        val swipePath = Path()
+        lateinit var startPoint: Point
+        lateinit var endPoint: Point
+
+        when (type) {
+            "UP" -> {
+                startPoint = Point(rightX.toInt(), topY.toInt())
+                endPoint = Point(rightX.toInt(), bottomY.toInt())
+            }
+
+            "DOWN" -> {
+                startPoint = Point(rightX.toInt(), bottomY.toInt())
+                endPoint = Point(rightX.toInt(), topY.toInt())
+            }
+
+            else -> { // default on a scroll up
+                startPoint = Point(rightX.toInt(), topY.toInt())
+                endPoint = Point(rightX.toInt(), bottomY.toInt())
+            }
+        }
+
+
+        swipePath.moveTo(startPoint.x.toFloat(), startPoint.y.toFloat())
+        swipePath.lineTo(endPoint.x.toFloat(), endPoint.y.toFloat())
+
+        val gestureBuilder = GestureDescription.Builder()
+        gestureBuilder.addStroke(GestureDescription.StrokeDescription(swipePath, 0, duration))
+
+        return gestureBuilder.build()
+
+//        if(type == "SCROLL_DOWN") {
+//        }
+
     }
 
     fun buildSwipe(
